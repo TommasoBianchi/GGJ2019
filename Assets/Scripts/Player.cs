0484387ec8e3469485deca153efdda0f369ab2e2
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2019-01-26 16:46:40 +0100

    Linked animator to player controller

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityTools.DataManagement;
+using System;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Player : MonoBehaviour
@@ -7,11 +8,19 @@ public class Player : MonoBehaviour
 
     [SerializeField]
     private int playerID;
+    [SerializeField]
+    private KeyBindings keyBindings;
+    [SerializeField]
+    private ShellStats baseShellStats;
 
     private ShellStats currentShellStats;
 
     private Rigidbody myRigidbody;
+    private Animator animator;
     private PressToGetShellUI pressToGetShellUI;
+
+    private KeyCode attackKeyCode;
+    private KeyCode defendKeyCode;
 
     private bool isInShellRange = false;
 
@@ -19,6 +28,8 @@ public class Player : MonoBehaviour
     {
         myRigidbody = GetComponent<Rigidbody>();
         pressToGetShellUI = Instantiate(ConstantsManager.PressToGetShellUIPrefab);
+        animator = GetComponentInChildren<Animator>();
+        currentShellStats = baseShellStats;
     }
 
     private void Update()
@@ -37,6 +48,13 @@ public class Player : MonoBehaviour
         {
             playerID = ID;
         }
+
+        bool keyCodeSetup = Enum.TryParse("Joystick" + playerID + "Button" + keyBindings.attackKeyCode, out attackKeyCode);
+        keyCodeSetup &= Enum.TryParse("Joystick" + playerID + "Button" + keyBindings.defendKeyCode, out defendKeyCode);
+        if (!keyCodeSetup)
+        {
+            Debug.LogError("Problem in setupping key codes for player" + playerID);
+        }
     }
 
     private void ReadInputs()
@@ -45,7 +63,9 @@ public class Player : MonoBehaviour
         float xAxis = Input.GetAxis("HorizontalJ" + playerID);
         float yAxis = Input.GetAxis("VerticalJ" + playerID);
 
-        myRigidbody.velocity = new Vector3(xAxis, 0, yAxis) * Time.deltaTime * 200;// currentShellStats.MovementSpeed;
+        animator.SetBool("Walking", xAxis != 0 || yAxis != 0);
+
+        myRigidbody.velocity = new Vector3(xAxis, 0, yAxis) * Time.deltaTime * currentShellStats.MovementSpeed;
         if (xAxis != 0 || yAxis != 0)
         {
             Quaternion targetRotation = Quaternion.LookRotation(new Vector3(xAxis, 0, yAxis), Vector3.up);
@@ -62,16 +82,33 @@ public class Player : MonoBehaviour
 
         // Grab shell
 
-        // TEST
-        if(Input.GetButtonDown("Joystick" + playerID + "A"))
-        {
-            Debug.Log("Player" + playerID + " pressed A");
-        }
+        // TEST        
+        //if (Input.GetKeyDown(attackKeyCode))
+        //{
+        //    Debug.Log("Player" + playerID + " pressed A");
+        //}
+        //if (Input.GetKeyDown(defendKeyCode))
+        //{
+        //    Debug.Log("Player" + playerID + " pressed B");
+        //}
         // TEST
 
         // Attack
+        if(currentShellStats.CanAttack && Input.GetKeyDown(attackKeyCode))
+        {
+            animator.SetTrigger("Attack");
+        }
 
         // Block
+        //if (currentShellStats.CanBlock && Input.GetKeyDown(defendKeyCode))
+        //{
+        //    animator.SetBool("Block", true);
+        //}
+        //else if (currentShellStats.CanBlock && Input.GetKeyUp(defendKeyCode))
+        //{
+        //    animator.SetBool("Block", false);
+        //}
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
