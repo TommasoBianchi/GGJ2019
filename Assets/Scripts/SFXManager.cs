44e95683d89ca934d2f202c785a95cfd4f641d83
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2019-01-27 15:06:30 +0100

    Finished SFXs

--- Assets/Scripts/SFXManager.cs
+++ Assets/Scripts/SFXManager.cs
@@ -12,13 +12,13 @@ public class SFXManager : MonoBehaviour
         CannonShoot,
         CannonImpact,
         CannonImpactBlock,
-        Footsteps,  // TODO: implement
+        Footsteps,
         EquipShell,
         Death,
         ButtonsSwitch, 
         ButtonsClick,  // TODO: implement
         ShellBreak,
-        PaguroVoice,  // TODO: implement
+        PaguroVoice,
         Win
     }
 
@@ -37,6 +37,8 @@ public class SFXManager : MonoBehaviour
     private Dictionary<SFXType, AudioClip[]> clipsByType;
     private AudioSource sfxSource;
 
+    private Dictionary<int, AudioSource> stepsAudioSources;
+
     private void Awake()
     {
         if (_instance == null)
@@ -50,6 +52,7 @@ public class SFXManager : MonoBehaviour
         }
 
         clipsByType = soundEffects.ToDictionary(holder => holder.type, holder => holder.clips);
+        stepsAudioSources = new Dictionary<int, AudioSource>();
 
         GetComponent<AudioSource>().volume = settings.musicVolume * musicVolumeMultiplier;
         sfxSource = gameObject.AddComponent<AudioSource>();
@@ -67,6 +70,34 @@ public class SFXManager : MonoBehaviour
         else
         {
             Debug.LogWarning("Trying to play an SFX of type " + type + " but SFXManager has no clips");
+        }
+    }
+
+    public static void PlayFootsteps(int playerID)
+    {
+        if (!_instance.stepsAudioSources.ContainsKey(playerID))
+        {
+            _instance.stepsAudioSources[playerID] = _instance.gameObject.AddComponent<AudioSource>();
+            _instance.stepsAudioSources[playerID].volume = _instance.settings.sfxVolume * _instance.sfxMusicMultiplier;
+            _instance.stepsAudioSources[playerID].playOnAwake = false;
+            _instance.stepsAudioSources[playerID].loop = true;
+            _instance.stepsAudioSources[playerID].clip = _instance.clipsByType[SFXType.Footsteps][0];
+        }
+
+        if (!_instance.stepsAudioSources[playerID].isPlaying)
+        {
+            _instance.stepsAudioSources[playerID].Play();
+        }
+    }
+
+    public static void StopFootsteps(int playerID)
+    {
+        if (_instance.stepsAudioSources.ContainsKey(playerID))
+        {
+            if (_instance.stepsAudioSources[playerID].isPlaying)
+            {
+                _instance.stepsAudioSources[playerID].Stop();
+            }
         }
     }
 
